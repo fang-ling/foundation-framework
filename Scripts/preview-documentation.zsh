2fe4e6a0c88bb4aff0fc2e849d7935b26fd91927
Fang Ling <61139150+fang-ling@users.noreply.github.com>
2026-03-07 14:34:33 +0800

    Add the script to preview the documentation

---
+++ Scripts/preview-documentation.zsh
@@ -0,0 +1,40 @@
+#!/bin/zsh
+
+##
+##  preview-documentation.zsh
+##  image-io-framework
+##
+##  Created by Fang Ling on 2026/3/7.
+##
+##  Licensed under the Apache License, Version 2.0 (the "License");
+##  you may not use this file except in compliance with the License.
+##  You may obtain a copy of the License at
+##
+##    http://www.apache.org/licenses/LICENSE-2.0
+##
+##  Unless required by applicable law or agreed to in writing, software
+##  distributed under the License is distributed on an "AS IS" BASIS,
+##  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+##  See the License for the specific language governing permissions and
+##  limitations under the License.
+##
+
+FRAMEWORK_NAME="FoundationFramework"
+SYMBOLS_FOLDER=".build/symbol-graphs"
+
+# Extract the symbols
+clang \
+  -extract-api \
+  --product-name=$FRAMEWORK_NAME \
+  -o .build/symbol-graphs/$FRAMEWORK_NAME.symbols.json \
+  -x c-header Sources/FoundationFrameworkEssentials/**/*.h \
+  -I . \
+  -isysroot $(xcrun --show-sdk-path) \
+  -F $(xcrun --show-sdk-path)/System/Library/Frameworks
+
+# Preview the documentation
+$(xcrun --find docc) \
+  preview \
+  Sources/FoundationFrameworkEssentials/Documentation.docc \
+  -o .build/.docc-build \
+  --additional-symbol-graph-dir .build/symbol-graphs
